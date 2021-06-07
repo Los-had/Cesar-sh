cd86b5acc6d34e08d32fe48db78169c6c8d36767
Miguel <>
2021-06-07 13:44:25 +0000

    commit final

---
+++ main.sh
@@ -0,0 +1,21 @@
+#!usr/bin/env bash
+
+# input dop usuario 
+read -p "O que deseja fazer criptografar ou descriptografar  (c/d)" choice
+
+#verificação das escolhas do usuario
+if [[ "$choice" == "c" ]]; then
+  #input para o texto que vai ser criptografado
+  read -p "Seu texto aqui:  " usertext
+  #deixa tudo em minusculo
+  usertext="${usertext,,}"
+  #faz a criptografia
+  echo "$usertext" | tr '[a-z]' '[x-za-w]'
+else 
+  #input para o texto que vai ser criptografado
+  read -p "Seu texto aqui:  " usrtext
+  #deixa tudo em minusculo
+  usrtext="${usrtext,,}"
+  #faz a criptografia
+  echo "$usrtext" | tr '[x-za-w]' '[a-z]'
+fi
